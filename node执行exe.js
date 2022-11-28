acf64cd95cd80eaee523596cc67942880d9daa95
boluo <daboluo5317@gmail.com>
2022-11-28 13:23:43 +0800

    feat: node执行其他应用

---
+++ node执行exe.js
@@ -0,0 +1,18 @@
+// 需要先有node环境
+const exec = require('child_process').execFile
+
+// 注意path参数中路径分隔符 \ 转义 \\
+const runExe = (path) => {
+	exec(path, (err, data) => {
+		console.log('err', err)
+		console.log('data', data)
+	})
+}
+
+const __main = () => {
+	runExe(
+		'C:\\Users\\boluo\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe'
+	)
+}
+
+__main()
