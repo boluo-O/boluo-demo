1e7eb9f7158b58723999ddfee9ba8e809f6a9a18
boluo <daboluo5317@gmail.com>
2022-11-29 11:00:38 +0800

    feat: js文件上传

---
+++ js文件上传前后端/server.js
@@ -0,0 +1,45 @@
+const http = require('http')
+const formidable = require('formidable')
+const fs = require('fs')
+
+http.createServer((req, res) => {
+	//  设置cors跨域
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+	res.setHeader('Content-Type', 'application/json')
+
+	//  保存和重命名文件
+	if (req.method === 'POST') {
+		const form = new formidable.IncomingForm()
+		form.uploadDir = './myDir'
+		form.keepExtensions = true
+
+		form.on('field', (field, value) => {
+			console.log('on field')
+			console.log('field', field)
+			console.log('value', value)
+		})
+
+		form.on('end', () => {
+			console.log('on end')
+			res.end('上传完成!')
+		})
+
+		form.on('file', (name, file) => {
+			console.log('on file')
+			fs.renameSync(file.filepath, './myDir/' + file.originalFilename)
+		})
+
+		form.on('error', (err) => {
+			console.log('err', err)
+			res.statusCode = 500
+			res.end('服务器内部错误!')
+		})
+
+		form.parse(req)
+	} else if (req.method === 'OPTIONS') {
+		res.setHeader('Access-Control-Allow-Origin', '*')
+		res.statusCode = 200
+		res.end()
+	}
+}).listen(8844, '127.0.0.1')
